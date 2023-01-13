1c9b7b27d4118772dae24bcd77179dbcde4e1bd7
Pablo Penalva <ppenalva@me.com>
2023-01-13 18:30:47 +0100

    primer cambio de desarrollo

--- GitPrueba/ContentView.swift
+++ GitPrueba/ContentView.swift
@@ -8,20 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isPresented = false
     var body: some View {
-        VStack {
+        
             NavigationView {
-                Button("Show Modal") {
-                    self.isPresented = true
-                }.foregroundColor(.red)
-                .sheet(isPresented: $isPresented) {
-                    ModelView()
-                }
+                NavigationLink(destination: ModelView()) {
+                    Text("Go next")
+            }
                 .navigationTitle("XCode and Git")
             }
         }
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
