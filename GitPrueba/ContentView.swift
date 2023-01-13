cd5358bcf7cf95e8831226349c1b58ad7853ea9a
Pablo Penalva <ppenalva@me.com>
2023-01-13 17:39:48 +0100

    cuarto cambio

--- GitPrueba/ContentView.swift
+++ GitPrueba/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isPresented = false 
+    @State private var isPresented = false
     var body: some View {
         VStack {
             NavigationView {
                 Button("Show Modal") {
                     self.isPresented = true
-                }
+                }.foregroundColor(.red)
                 .sheet(isPresented: $isPresented) {
                     ModelView()
                 }
