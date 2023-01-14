616e74b0205f754ae9af2966b47fee922cd37144
Pablo Penalva <ppenalva@me.com>
2023-01-14 10:59:51 +0100

    cambio de color

--- GitPrueba/ContentView.swift
+++ GitPrueba/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             NavigationView {
                 NavigationLink(destination: ModelView()) {
                     Text("Go next")
-                }.foregroundColor(.red)
+                }.foregroundColor(.purple)
                 .navigationTitle("XCode and Git")
             }
         }
